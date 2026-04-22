42588d614d66c91065203260eac91ac34f0bd8f2
Ridam Gupta <196336262+rhythmgupta19@users.noreply.github.com>
2026-04-22 19:52:08 +0530

    Adding one new programm file

---
+++ Array/Programm6.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.Scanner;
+public class Programm6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        for(int i =0;i <size;i++){
+            System.out.println("Enter the elements");
+            arr[i] = sc.nextInt();
+        }
+        int product = 1;
+        System.out.println("Display elements and product");
+        for(int i :arr) {
+            product *= i;
+            System.out.print(i +" ");
+        }
+        System.out.println("Product " + product);
+        sc.close();
+    }
+}
